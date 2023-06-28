1da9937e25eefe3a849dcca38d021411341ee2c8
User <User@network>
2023-06-28 18:55:15 +0300

    Add project files.

---
+++ Dmizh.Classes/Program.cs
@@ -0,0 +1,29 @@
+﻿
+Person tom = new Person();
+
+tom.Print();
+
+class Person
+{
+    public string name;
+    public int age;
+    public Person()
+    {
+        name = "Undefined";
+        age = 18;
+    }
+    public Person (string n)
+    {
+        name = n;
+        age = 18;
+    }
+    public Person(string n, int a)
+    {
+        name = n;
+        age = a;
+    }
+    public void Print()
+    {
+        Console.WriteLine($"Имя: {name}  Возраст: {age}");
+    }
+}
